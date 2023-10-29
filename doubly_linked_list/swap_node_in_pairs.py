a766e3decf1dcb00c9e83c3c27158385f5066a6e
Abhishek Biradar <abhishekbiradar0207@gmail.com>
2023-10-29 16:19:54 +0530

    Swap the node in pairs

---
+++ doubly_linked_list/swap_node_in_pairs.py
@@ -0,0 +1,78 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+        
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.length = 1
+
+    def print_list(self):
+        output = []
+        current_node = self.head
+        while current_node is not None:
+            output.append(str(current_node.value))
+            current_node = current_node.next
+        print(" <-> ".join(output))
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            temp = self.head
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = new_node
+            new_node.prev = temp
+        self.length += 1
+        return True
+
+    def swap_pairs(self):
+        if self.length > 1:
+            temp = self.head
+            self.head = temp.next
+            while temp and temp.next is not None:
+                after = temp.next
+                if temp.prev:
+                    temp.prev.next = after
+                after.prev = temp.prev
+                temp.next = after.next
+                after.next = temp
+                temp.prev = after
+                if temp.next:
+                    temp.next.prev = temp
+                temp = temp.next
+            return True
+
+
+
+my_dll = DoublyLinkedList(1)
+my_dll.append(2)
+my_dll.append(3)
+my_dll.append(4)
+
+print('my_dll before swap_pairs:')
+my_dll.print_list()
+
+my_dll.swap_pairs() 
+
+
+print('my_dll after swap_pairs:')
+my_dll.print_list()
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    my_dll before swap_pairs:
+    1 <-> 2 <-> 3 <-> 4
+    ------------------------
+    my_dll after swap_pairs:
+    2 <-> 1 <-> 4 <-> 3
+
+"""
